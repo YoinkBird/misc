700785503dc7c7e8b3bc23f3ac6a629a022f7928
yoinkbird <YoinkBird2010@gmail.com>
2018-07-18 22:06:48 -0500

    adding logs for troubleshooting thunderbolt egpu setup

---
+++ os/linux/egpu/steps_post_mv.sh
@@ -0,0 +1,20 @@
+00:02.0 VGA compatible controller: Intel Corporation UHD Graphics 620 (rev 07)
+	Subsystem: Hewlett-Packard Company UHD Graphics 620
+	Kernel driver in use: i915
+	Kernel modules: i915
+00:04.0 Signal processing controller: Intel Corporation Skylake Processor Thermal Subsystem (rev 08)
+	Subsystem: Hewlett-Packard Company Xeon E3-1200 v5/E3-1500 v5/6th Gen Core Processor Thermal Subsystem
+--
+00:13.0 Non-VGA unclassified device: Intel Corporation Sunrise Point-LP Integrated Sensor Hub (rev 21)
+	Subsystem: Hewlett-Packard Company Sunrise Point-LP Integrated Sensor Hub
+	Kernel driver in use: intel_ish_ipc
+	Kernel modules: intel_ish_ipc
+00:14.0 USB controller: Intel Corporation Sunrise Point-LP USB 3.0 xHCI Controller (rev 21)
+	Subsystem: Hewlett-Packard Company Sunrise Point-LP USB 3.0 xHCI Controller
+--
+08:00.0 VGA compatible controller: NVIDIA Corporation GP104 [GeForce GTX 1070] (rev a1)
+	Subsystem: Gigabyte Technology Co., Ltd GP104 [GeForce GTX 1070]
+	Kernel driver in use: nvidia
+	Kernel modules: nvidiafb, nouveau, nvidia_drm, nvidia
+08:00.1 Audio device: NVIDIA Corporation GP104 High Definition Audio Controller (rev a1)
+	Subsystem: Gigabyte Technology Co., Ltd GP104 High Definition Audio Controller
