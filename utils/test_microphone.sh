72aa15b59b612e3df75caeabf7ee166d072bc1d0
YoinkBird <YoinkBird2010@gmail.com>
2023-07-27 13:28:18 -0500

    util: test recording device, hacky version 2/n

--- utils/test_microphone.sh
+++ utils/test_microphone.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 set -x
+set -ueo pipefail
 # https://linuxconfig.org/how-to-test-microphone-with-audio-linux-sound-architecture-alsa
 # '-c 2' : https://unix.stackexchange.com/questions/192554/issues-with-arecord-command-channels-count-non-available
 
 arecord -l
-arecord -f S16_LE -d 10 -r 16000 --device="hw:2,0" -c 2 /tmp/test-mic.wav \
+set +x
+read -p "which device? enter as '<card number>:<device N>' " device
+set -x
+# https://explainshell.com/explain?cmd=arecord+-f+S16_LE+-d+10+-r+16000+--device%3D%22hw%3A1%2C0%22+%2Ftmp%2Ftest-mic.wav
+# 0: run until it is killed; not feasible for this script
+arecord -f S16_LE -d 30 -r 16000 --device="${device}" -c 2 /tmp/test-mic-${device}.wav \
   || fuser -fv /dev/snd/*
 
-aplay /tmp/test-mic.wav && rm /tmp/test-mic.wav
+aplay /tmp/test-mic-${device}.wav && rm /tmp/test-mic-${device}.wav
 
 # troubleshooting:
 # "device or resource busy" : http://www.noah.org/wiki/%22arecord:_..._audio_open_error:_Device_or_resource_busy%22
