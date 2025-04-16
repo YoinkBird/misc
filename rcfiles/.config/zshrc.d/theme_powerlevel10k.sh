81333a081d9d57bff350e37f4a39dbe980a50bc0
TODO <TODO>
2025-04-16 13:34:34 +0200

    setup: adds config for zshrc, currently biased towards iterm2

---
+++ rcfiles/.config/zshrc.d/theme_powerlevel10k.sh
@@ -0,0 +1,2 @@
+test -e "$(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" && \
+  source "$(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme"
---
+++ setup/macos/iterm2/configure_iterm2.sh
@@ -0,0 +1,2 @@
+# https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#installation
+brew install powerlevel10k
