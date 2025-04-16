f366aefdad43f3468ccedb15fa1c9094bc5190bc
TODO <TODO>
2025-04-16 12:09:58 +0200

    experiment: iterm2 settings

---
+++ setup/macos/iterm2/worklog.sh
@@ -0,0 +1,14 @@
+cat <<EOM > /dev/null
+# PURPOSE:
+# https://medium.com/seokjunhong/customize-the-terminal-zsh-iterm2-powerlevel10k-complete-guide-for-beginners-35c4ba439055
+EOM
+
+defaults read com.googlecode.iterm2 > backup_current.plist
+# convert plist to json: https://getpenguin.app/knowledge-base/plist-to-json/
+# vvv creates file called: <stdin>
+# defaults read com.googlecode.iterm2 | plutil -convert json -
+# => somehow, it is literally creating files called '<stdin>' and 'stdout', therefore need to explicitly specify the paths
+# man plutil
+# Specifying - as an input file reads from stdin.
+# Specifying - as the path outputs to stdout.
+defaults read com.googlecode.iterm2 | plutil -convert json -o - - |jq
