411c4d9a8edfcbff29b17d10f4f8f099079f2234
TODO <TODO>
2025-04-16 12:29:25 +0200

    experiment: iterm2 settings - remove completely, install again

--- setup/macos/iterm2/worklog.sh
+++ setup/macos/iterm2/worklog.sh
@@ -17,5 +17,42 @@ defaults read com.googlecode.iterm2 | plutil -convert json -o - - |jq
 
 # delete all settings, compare against previous state
 cat <<EOM > /dev/null
-# ONE-OFF, don't do this again! # defaults delete com.googlecode.iterm2
+---
+# ONE-OFF, don't do this again!
+% defaults delete com.googlecode.iterm2
+
+---
+Not sure whether settings actually got reset:
+after deleting defaults, the following option was still set:
+'left ⌥ key acts as +ESC'
+but it was thought to have been "not the default"
+
+Therefore, going to completely uninstall and check the resulting settings:
+
+% brew uninstall iterm2
+
+Following is left:
+
+# manual modify output, no clean fast way to just show the link => target
+% ls -ltr ~/.config/iterm2 
+total 0
+AppSupport -> /Users/<>/Library/Application Support/iTerm2
+sockets
+
+% tree ~/"Library/Application Support/iTerm2"
+/Users/<>/Library/Application Support/iTerm2
+├── DynamicProfiles
+├── iterm2-daemon-1.socket
+├── iterm2-daemon-1.socket.lock
+├── iTermServer-3.5.13
+├── SavedState
+│   ├── lock
+│   ├── restorable-state.sqlite
+│   ├── restorable-state.sqlite-shm
+│   └── restorable-state.sqlite-wal
+├── Scripts
+└── version.txt
+
+
+
 EOM
