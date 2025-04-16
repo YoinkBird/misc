e64a09cedaa0d4d41d7a66e0783e27491c482a7d
TODO <TODO>
2025-04-16 12:11:10 +0200

    experiment: iterm2 settings - reset to default

--- setup/macos/iterm2/worklog.sh
+++ setup/macos/iterm2/worklog.sh
@@ -3,6 +3,7 @@ cat <<EOM > /dev/null
 # https://medium.com/seokjunhong/customize-the-terminal-zsh-iterm2-powerlevel10k-complete-guide-for-beginners-35c4ba439055
 EOM
 
+# capture current settings after much manual modification
 defaults read com.googlecode.iterm2 > backup_current.plist
 # convert plist to json: https://getpenguin.app/knowledge-base/plist-to-json/
 # vvv creates file called: <stdin>
@@ -12,3 +13,9 @@ defaults read com.googlecode.iterm2 > backup_current.plist
 # Specifying - as an input file reads from stdin.
 # Specifying - as the path outputs to stdout.
 defaults read com.googlecode.iterm2 | plutil -convert json -o - - |jq
+
+
+# delete all settings, compare against previous state
+cat <<EOM > /dev/null
+# ONE-OFF, don't do this again! # defaults delete com.googlecode.iterm2
+EOM
